767f2035676736d13711e88f0f8733d4d8fea393
raspi_voice <158295465+CodeNube737@users.noreply.github.com>
2025-03-14 12:15:43 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ int main() {
     cv::Mat image = cv::Mat::zeros(300, 600, CV_8UC3);
     cv::putText(image, "Hello, OpenCV!", cv::Point(50, 150), cv::FONT_HERSHEY_SIMPLEX, 1, cv::Scalar(0, 255, 0), 2);
     cv::imshow("Test", image);
+    std::cout << "Hello World!\n"; // added to learn gitHub better
     cv::waitKey(0);
     return 0;
 }
